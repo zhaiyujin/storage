488f58f4733588d37f5eabed91e5ac7f385695a9
TonicGB <568941179@qq.com>
2021-04-27 16:03:43 +0800

    x

--- oss_test.go
+++ oss_test.go
@@ -10,24 +10,18 @@ import (
 	"testing"
 )
 
-/**
-真曹丹
- */
+
 func BenchmarkNewStorage(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, err := NewStorage()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
+		storage:= NewStorage()
+		fmt.Println(storage)
 		//	fmt.Println(storage)
 	}
 }
 
 func TestStorage(t *testing.T) {
-	storage, err := NewStorage()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	storage := NewStorage()
+
 	fmt.Println(storage)
 	/*filename, err := storage.PutObjectFromFile("jpeg", "2.jpeg")
 	if err != nil {
